fe9ab13bd2e030ed1c3c923cfd271970b263e767
Adrien Baur <abaur@student.42.fr>
2020-01-03 12:00:07 +0100

    Tester now capable of reading a file

--- main.c
+++ main.c
@@ -6,43 +6,50 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/30 14:54:44 by abaur             #+#    #+#             */
-/*   Updated: 2020/01/03 11:00:47 by abaur            ###   ########.fr       */
+/*   Updated: 2020/01/03 11:47:41 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+
 #include "logutil/logutil.h"
 int	get_next_line(int, char**);
 
-void TestOneGNL(){
+int TestOneGNL(int fd){
 	char* line = NULL;
-	int r;
+	int err;
 
-	r = get_next_line(0, &line);
+	err = get_next_line(fd, &line);
 
-	if (r == 1) {
+	if (-1 < err) {
 		printf("%s\n", line);
 		for (line; *line; line++){
 			if (*line == '\n' || *line == EOF || *line == 0)
-				printfc(YELLOW, 1, "%02X ", *line);
+				printfc(CYAN, 1, "%02X ", *line);
 			else
 				printf ("%02X ", *line);
 		}
 		printf("\n");
 	}
 	else
-		printf("Unexpected return value: %d\n", r);
+		printf("Unexpected return value: %d\n", err);
+
+	return err;
 }
 
 int	main(int argc, char **args){
-	int count;
-	
-	count = atoi(args[1]);
-	if (count < 1)
-		count = 1;
+	int fd = 0;
 
-	for (int i=0; i<count; i++){
-		TestOneGNL();
+	if (argc > 1)
+		fd = open(args[1], O_RDONLY);
+	if (fd < 0){
+		printfc(YELLOW, 1, "Error opening file: %#x", errno);
+		return -1;
 	}
+
+	while(0 < TestOneGNL(fd));
+
 }
